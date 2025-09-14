68c32a71d322c80c9cbe8000322cf721958d5902
Brian Widhalm <bdwidhalm@gmail.com>
2025-09-14 13:58:53 -0600

    pulling song data into REST call

--- src/main/java/com/widhalm/music_manager_service/controller/MusicController.java
+++ src/main/java/com/widhalm/music_manager_service/controller/MusicController.java
@@ -1,4 +1,25 @@
 package com.widhalm.music_manager_service.controller;
 
+import com.widhalm.music_manager_service.model.Song;
+import com.widhalm.music_manager_service.repository.MusicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/music")
 public class MusicController {
+
+    @Autowired
+    MusicRepository musicRepository;
+
+    @GetMapping("/songs")
+    public String getAllSongs() {
+        List<Song> songs = musicRepository.allSongs();
+        System.out.println("Count:  " + musicRepository.count());
+        return songs.toString();
+    }
 }
