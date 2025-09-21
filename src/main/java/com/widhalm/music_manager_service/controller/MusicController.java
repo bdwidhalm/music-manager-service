50bc2ed567b92d5f51cfb4443ea13e2edaf18e88
Brian Widhalm <bdwidhalm@gmail.com>
2025-09-21 16:53:29 -0600

    enabling CORS

--- src/main/java/com/widhalm/music_manager_service/controller/MusicController.java
+++ src/main/java/com/widhalm/music_manager_service/controller/MusicController.java
@@ -5,6 +5,7 @@ import com.widhalm.music_manager_service.repository.MusicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class MusicController {
     @Autowired
     MusicRepository musicRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/songs")
     public ResponseEntity<List<Song>> getAllSongs() {
         List<Song> songs = musicRepository.allSongs();
