884551877f1c209e8b5a42172cf81b87c17fef4c
Brian Widhalm <bdwidhalm@gmail.com>
2025-09-21 09:55:59 -0600

    project init

---
+++ src/main/java/com/widhalm/music_manager_service/MusicManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.widhalm.music_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/widhalm/music_manager_service/MusicManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widhalm.music_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
